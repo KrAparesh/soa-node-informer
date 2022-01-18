d0ca64a4a2433defff86737d9d551573f35941ee
APARESH <apareshuif@gmail.com>
2022-01-18 18:00:32 +0530

    updated sandbox stats

--- soa.js
+++ soa.js
@@ -21,7 +21,7 @@ var examdateprev = "";
 
 async function configureBrowser() {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ['--no-sandbox','--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
